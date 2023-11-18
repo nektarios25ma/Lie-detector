9a01ac0007173d414bf15f22d14435195e7a4eaf
nektarios.25ma <nektarios.25ma2@gmail.com>
2023-11-18 21:26:13 +0200

    Add files via upload

---
+++ TESTNANOanalogWriteTO_ESP.ino
@@ -0,0 +1,39 @@
+/*
+  Analog input, analog output, serial output
+
+  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
+  the result to set the pulse width modulation (PWM) of an output pin.
+  Also prints the results to the Serial Monitor.
+
+  The circuit:
+  - potentiometer connected to analog pin 0.
+    Center pin of the potentiometer goes to the analog pin.
+    side pins of the potentiometer go to +5V and ground
+  - LED connected from digital pin 9 to ground through 220 ohm resistor
+
+  created 29 Dec. 2008
+  modified 9 Apr 2012
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogInOutSerial
+*/
+
+// These constants won't change. They're used to give names to the pins used:
+const int analogOUTPin = A0;  // Analog input pin that the potentiometer is attached to
+
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+  pinMode(analogOUTPin,OUTPUT);
+}
+
+void loop() {
+  
+  analogWrite(analogOUTPin, 168);
+
+  
+  delay(20000);
+}
